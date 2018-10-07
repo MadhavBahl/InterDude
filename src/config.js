321f76008314f4ddc61fe3035f6f6136bde240de
Madhav Bahl <madhavbahl10@gmail.com>
2018-10-07 10:40:00 +0530

    Test changes for hotspot

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
 	enableCors: true,
-	port: process.env.PORT || 8000,
+	port: process.env.PORT || 8080,
 	db_url: 'mongodb://admin:interdude1@ds217002.mlab.com:17002/interdude',
 	saltRounds: 10
 }
